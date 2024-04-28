3f4d8cbe69da856a39dc209ad219bd0a042e166a
Coffee-Mocha-1205 <154251261+Coffee-Mocha-1205@users.noreply.github.com>
2024-04-29 01:28:44 +0800

    Update translate.js

--- scripts/cmds/translate.js
+++ scripts/cmds/translate.js
@@ -128,7 +128,7 @@ async function translateAndSendMessage(content, langCodeTrans, message, getLang)
     try {
         const { text, lang } = await translate(content.trim(), langCodeTrans.trim());
 
-        const header = `━━━━━━━━━━━━━━━━\n${text}\n\nTranslated from ${lang} to ${langCodeTrans}\n━━━━━━━━━━━━━━━━`;
+        const header = `━━━━━━━━━━━━━━━━\n▫️$ {text}\n\n🪧 Translated from ${lang} to ${langCodeTrans}\n━━━━━━━━━━━━━━━━`;
 
         await message.reply(header);
     } catch (error) {
